36d01e346725ea7edabfc13141461990071560a6
indhu yelineni <indhusworkmail@gmail.com>
2023-05-26 15:53:08 +0530

    created http interceptors

---
+++ src/app/interceptors/http-headers.interceptor.ts
@@ -0,0 +1,29 @@
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class HttpHeadersInterceptor implements HttpInterceptor {
+  constructor() {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    req = req.clone({
+      setHeaders: {
+        'x-rapidapi-key': 'e7f2df36amsh9f952a964c8c5dep107269jsn79ff744a05eb',
+        'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com',
+      },
+      setParams: {
+        key: 'b94348c2b3e643a093537a24996f92e6',
+      },
+    });
+    return next.handle(req);
+  }
+}
